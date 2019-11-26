fb4c14aaf4f0c064930fe7e170b6e80c32f4e893
lcreid <lcreid@wpi.edu>
2019-11-25 20:09:17 -0500

    new API deploy 2.3

--- VisionOfHope/js/api.js
+++ VisionOfHope/js/api.js
@@ -1,6 +1,6 @@
 // all access driven through BASE. Must end with a SLASH
 // be sure you change to accommodate your specific API Gateway entry point
-var base_url = " https://r4p7e3f5f7.execute-api.us-east-1.amazonaws.com/G_2_2"; 
+var base_url = "  https://r4p7e3f5f7.execute-api.us-east-1.amazonaws.com/G_2_3"; 
 
 var videos_url    = base_url + "videoSegments";   // GET
 var playlists_url = base_url + "playlists";     // GET
