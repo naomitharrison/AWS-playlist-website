a87eb23ddc000895fee96b3e9b5a39cf061b9f21
naomitharrison <ntharrison@wpi.edu>
2019-11-26 16:32:53 -0500

    append playlist lambda

--- VisionOfHope/src/main/java/cs3733/main/DB/PlaylistsDAO.java
+++ VisionOfHope/src/main/java/cs3733/main/DB/PlaylistsDAO.java
@@ -145,4 +145,11 @@ public class PlaylistsDAO {
 		return false;
 	}
 
+
+
+	public boolean addVideoToPlaylist(String playlistName, String videoURL) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
