9eb9a961b5fd619ec0d20f2404c00fe660264038
naomitharrison <ntharrison@wpi.edu>
2019-11-26 15:44:56 -0500

    delete video segment working except for the database stuff

--- VisionOfHope/src/main/java/cs3733/main/http/DeleteVideoSegmentResponse.java
+++ VisionOfHope/src/main/java/cs3733/main/http/DeleteVideoSegmentResponse.java
@@ -1,5 +1,28 @@
 package cs3733.main.http;
 
 public class DeleteVideoSegmentResponse {
+	public final String name;
+	public final int statusCode;
+	public final String error;
+
+	public DeleteVideoSegmentResponse(String name, int statusCode) {
+		this.name = name;
+		this.statusCode = statusCode;
+		this.error = "";
+	}
+
+	public DeleteVideoSegmentResponse(String name, int statusCode, String errorMessage) {
+		this.statusCode = statusCode;
+		this.error = errorMessage;
+		this.name = name;
+	}
+
+	public String toString() {
+		if (statusCode / 100 == 2) { // too cute?
+			return "DeleteVideoSegmentResponse(" + name + ")";
+		} else {
+			return "DeleteVideoSegmentErrorResult(" + name + ", statusCode=" + statusCode + ", err=" + error + ")";
+		}
+	}
 
 }
