dd9eec1add5e81b39d8b47e1b3df360194dc4b84
naomitharrison <ntharrison@wpi.edu>
2019-11-29 10:19:13 -0500

    search should work

--- VisionOfHope/src/main/java/cs3733/main/http/VideoSegmentSearchRequest.java
+++ VisionOfHope/src/main/java/cs3733/main/http/VideoSegmentSearchRequest.java
@@ -1,5 +1,20 @@
 package cs3733.main.http;
 
 public class VideoSegmentSearchRequest {
+	final String character;
+	final String string;
+	
+	public String getCharacter() {return character; }
+	public String getString() {return string; }
+	public String getSearch() { 
+		String response;
+		response = character + " , " + string;
+		return response;
+	}
+	
+	public VideoSegmentSearchRequest (String ch, String st) {
+		this.character = ch;
+		this.string = st;
+	}
 
 }
