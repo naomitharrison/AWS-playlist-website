13193e03bcdcd34f70ecbf76a2248eefaf7f9677
Teresa Saddler <tsaddler@wpi.edu>
2019-12-01 17:33:13 -0500

    Add functionality for fetching list of remote libraries

--- VisionOfHope/js/api.js
+++ VisionOfHope/js/api.js
@@ -4,4 +4,6 @@ var base_url = "    https://r4p7e3f5f7.execute-api.us-east-1.amazonaws.com/G_2_5
 
 var videos_url    = base_url + "videoSegments";   // GET
 var playlists_url = base_url + "playlists";     // GET
+var playlistVideoSegments_url = base_url + "playlistVideoSegments"; // POST
+var remoteLibraries_url = base_url + "remoteLibrary"; // GET + POST
 
---
+++ VisionOfHope/js/get_remoteLibraries.js
@@ -0,0 +1,36 @@
+function refreshRemoteLibraries() {
+	let request = new XMLHttpRequest();
+	request.open('GET', remoteLibraries_url, true);
+	request.send();
+
+	console.log("sent");
+
+	request.onload = function() {
+		if (request.readyState == XMLHttpRequest.DONE) {
+			console.log ("request:" + request.responseText);
+			processRemoteLibrariesListResponse(request.responseText);
+		} else {
+			processRemoteLibrariesListResponse("N/A");
+		}
+	};
+}
+
+function processRemoteLibrariesListResponse(result) {
+	console.log("res:" + result);
+	var js = JSON.parse(result);
+	var remoteLibraryList = document.getElementById('remoteLibraryList');
+
+	var output = '';
+	output +='<ul style="list-style-type:none;">';
+	for (var i = 0; i < js.listOfRemoteLibraries.length; i++) {
+		var constantJson = js.listOfRemoteLibraries[i];
+		console.log(constantJson);
+
+		var cname = constantJson["name"];
+		output += '<li><input type="radio" name="remoteLibrary" value="' + cname + '">' + cname + '</li>';
+	}
+	output += '</ul>';
+	console.log("final HTML: " + output);
+	remoteLibraryList.innerHTML = output;
+}
+
