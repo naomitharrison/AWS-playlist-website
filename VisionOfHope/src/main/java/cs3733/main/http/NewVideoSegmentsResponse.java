aaa25fdd6764ed5bb168025a922410e3201ce79d
Zack <zack7699@gmail.com>
2019-12-01 20:36:07 -0500

    new video handler

--- VisionOfHope/src/main/java/cs3733/main/http/NewVideoSegmentsResponse.java
+++ VisionOfHope/src/main/java/cs3733/main/http/NewVideoSegmentsResponse.java
@@ -1,5 +1,26 @@
 package cs3733.main.http;
 
 public class NewVideoSegmentsResponse {
+	public final String name;
+	public final int statusCode;
+	public final String error;
 
+	public NewVideoSegmentsResponse(String name, int statusCode) {
+		this.name = name;
+		this.statusCode = statusCode;
+		this.error = "";
+	}
+
+	public NewVideoSegmentsResponse(String name, int statusCode, String errorMessage) {
+		this.statusCode = statusCode;
+		this.error = errorMessage;
+		this.name = name;
+	}
+
+	public String toString() {
+		if (statusCode / 100 == 2) { // too cute?
+			return "NewVideoSegmentsResponse(" + name + ")";
+		} else {
+			return "NewVideoSegmentsResponse(" + name + ", statusCode=" + statusCode + ", err=" + error + ")";
+		
 }
